fa23217f2d9671a284cd649572ff4ebb1f180a54
Joris Ebbelaar <joris@datalumina.com>
2025-12-18 13:18:20 +0100

    Cleanup

--- app/workflows/workflow_registry.py
+++ app/workflows/workflow_registry.py
@@ -1,9 +1,7 @@
 from enum import Enum
 
-from workflows.example_customer_care_workflow import ExampleCustomerCareWorkflow
 from workflows.example_streaming_workflow import ExampleStreamingWorkflow
 
 
 class WorkflowRegistry(Enum):
     EXAMPLE_STREAMING_WORKFLOW = ExampleStreamingWorkflow
-    EXAMPLE_CUSTOMER_CARE_WORKFLOW = ExampleCustomerCareWorkflow
